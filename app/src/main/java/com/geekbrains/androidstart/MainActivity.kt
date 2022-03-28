576659bfe3f58d89cb012ae3be9f94ed74c9744b
Ruslan Bagautdinov <rus-bagautdinov@mail.ru>
2022-03-28 13:52:19 +0500

    lesson 3 calc button onClick + switch display orientation

--- app/src/main/java/com/geekbrains/androidstart/MainActivity.kt
+++ app/src/main/java/com/geekbrains/androidstart/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.geekbrains.androidstart
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.calc)
+
+        initView()
+    }
+
+    override fun onSaveInstanceState(instanceState: Bundle) {
+        super.onSaveInstanceState(instanceState)
+        val calcText = CalcText(textView.text.toString())
+        instanceState.putParcelable("text", calcText)
+    }
+
+    override fun onRestoreInstanceState(instanceState: Bundle) {
+        super.onRestoreInstanceState(instanceState)
+        val textCalcGet = instanceState.getParcelable<Parcelable>("text") as CalcText
+        textView.text = textCalcGet.text.toString()
+    }
+
+    private fun initView() {
+        textView = findViewById(R.id.calc_text)
+        val buttons = arrayOfNulls<MaterialButton>(16)
+        for (i in 0..15) {
+            val id = resources.getIdentifier("calc_btn_$i", "id", packageName)
+            buttons[i] = findViewById(id)
+            buttons[i]?.setOnClickListener {
+                textView.append(buttons[i]?.text)
+            }
+        }
     }
 }
