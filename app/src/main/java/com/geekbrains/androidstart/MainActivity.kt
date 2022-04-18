ab67a525a88e3534e89ce81730ef09f42fc373bd
Ruslan Bagautdinov <rus-bagautdinov@mail.ru>
2022-04-18 17:03:03 +0500

    lesson 4

--- app/src/main/java/com/geekbrains/androidstart/MainActivity.kt
+++ app/src/main/java/com/geekbrains/androidstart/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.geekbrains.androidstart
 
 import android.os.Bundle
+import android.os.Parcelable
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var navController: NavController
+    lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.calc)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
+        initView()
+    }
+
+    override fun onSaveInstanceState(instanceState: Bundle) {
+        super.onSaveInstanceState(instanceState)
+        val calcText = CalcText(textView.text.toString())
+        instanceState.putParcelable("text", calcText)
+    }
+
+    override fun onRestoreInstanceState(instanceState: Bundle) {
+        super.onRestoreInstanceState(instanceState)
+        val textCalcGet = instanceState.getParcelable<Parcelable>("text") as CalcText
+        textView.text = textCalcGet.text.toString()
+    }
+
+    private fun initView() {
+        textView = findViewById(R.id.calc_text)
+        val buttons = arrayOfNulls<MaterialButton>(16)
+        for (i in 0..15) {
+            val id = resources.getIdentifier("calc_btn_$i", "id", packageName)
+            buttons[i] = findViewById(id)
+            buttons[i]?.setOnClickListener {
+                textView.append(buttons[i]?.text)
+            }
+        }
     }
 }
